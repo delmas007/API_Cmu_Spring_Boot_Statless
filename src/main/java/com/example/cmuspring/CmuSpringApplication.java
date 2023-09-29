b40b6db1d023fe200e58dd9ca67d8ffd23437b37
delmas <azertyazerty1ze@gmail.com>
2023-09-29 22:35:17 +0000

    commit

---
+++ src/main/java/com/example/cmuspring/CmuSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.cmuspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmuSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmuSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cmuspring/CmuSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cmuspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmuSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
