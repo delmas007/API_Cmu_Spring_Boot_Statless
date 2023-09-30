e6c65dc99be2e7232c387df6c697fba7ba8ca8a9
delmas <azertyazerty1ze@gmail.com>
2023-09-30 12:14:35 +0000

    repository

---
+++ src/main/java/com/example/cmuspring/Repository/ConsultationRepository.java
@@ -0,0 +1,7 @@
+package com.example.cmuspring.Repository;
+
+import com.example.cmuspring.Model.Consultation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultationRepository extends JpaRepository<Integer, Consultation> {
+}
