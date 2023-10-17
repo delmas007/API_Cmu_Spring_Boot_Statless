d2cba36f129f7756ce0724867196bc6f44896640
delmas <azertyazerty1ze@gmail.com>
2023-10-17 00:37:04 +0000

    config jwt

--- src/main/java/com/example/cmuspring/Controller/ApiConnexion.java
+++ src/main/java/com/example/cmuspring/Controller/ApiConnexion.java
@@ -1,13 +1,16 @@
 package com.example.cmuspring.Controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 import static com.example.cmuspring.Utils.Constants.Api;
 
 public interface ApiConnexion {
 
     @PostMapping(Api+"/connexion")
-    Void Connexion(@RequestParam(required = false) String username,
-                   @RequestParam(required = false) String password);
+    Map<String, String> Connexion(@RequestParam(required = false) String username,
+                                                         @RequestParam(required = false) String password);
 }
