9f14e2470d19ce26984b1d6b224b5fc9b29ff20f
delmas007 <azertyazerty1ze@gmail.com>
2024-05-07 17:20:38 +0000

    security cros

--- src/main/java/com/example/cmuspring/Model/Role.java
+++ src/main/java/com/example/cmuspring/Model/Role.java
@@ -1,7 +1,6 @@
 package com.example.cmuspring.Model;
 
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
