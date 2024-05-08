320df9c365ec7fb51fa3ac31607a9e054910be7e
delmas007 <azertyazerty1ze@gmail.com>
2024-05-08 16:53:06 +0000

    security cros

--- src/main/java/com/example/cmuspring/Config/SpringConfig.java
+++ src/main/java/com/example/cmuspring/Config/SpringConfig.java
@@ -88,12 +88,9 @@ public class SpringConfig {
                         .authenticated()
                 )
                 .csrf(AbstractHttpConfigurer::disable)
-//                .cors(Customizer.withDefaults())
-//                .csrf(Customizer.withDefaults())
                 .sessionManagement(sess->sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .oauth2ResourceServer(oauth2->oauth2.jwt(Customizer.withDefaults()))
                 .cors(Customizer.withDefaults())
-//                .httpBasic(Customizer.withDefaults())
                 .userDetailsService(userDetailServiceImp);
 
         return httpSecurity.build();
